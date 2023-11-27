d05b9bd911f93b10400525d9425a22bf30b99b03
Malek Mahmoud <125380091+malekrouk@users.noreply.github.com>
2023-11-28 00:11:06 +0200

    few comments

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <vector>
 using namespace std;
 
-const int arraySize = 6668; // array size
 
 struct obj { // node
     float value;
@@ -13,20 +12,20 @@ struct obj { // node
 
 // heapify algorithm for max heap
 void heapifyMax(vector<obj>& v, int n, int i) {
-    int max = i; // parent
-    int l = 2 * i + 1; // left child
-    int r = 2 * i + 2; // right child
+    int max=i; // parent
+    int l=2*i+1; // left child
+    int r=2*i+2; // right child
 
     // left child >> parent
-    if (l < n && v[l].value > v[max].value)
-        max = l;
+    if (l<n && v[l].value > v[max].value)
+        max=l;
 
     // right child >> parent
-    if (r < n && v[r].value > v[max].value)
-        max = r;
+    if (r<n && v[r].value > v[max].value)
+        max=r;
 
     // parent is not the largest
-    if (max != i) {
+    if (max!=i) {
         swap(v[i], v[max]); // therefore swap with the largest child
 
         // another subtree
@@ -35,80 +34,49 @@ void heapifyMax(vector<obj>& v, int n, int i) {
 }
 
 // ascending order using heapify
-void buildMaxHeap(vector<obj>& heap) {
+void buildMaxHeap(vector<obj>& heap)
+{
     // build max-heap
-    for (int i = (heap.size()-1) / 2 - 1; i >= 0; i--)
+    for (int i=(heap.size()-1) / 2-1; i >= 0; i--)
         heapifyMax(heap, heap.size(), i);
 }
 
 
 
 // heapify algorithm for min heap
-void heapifyMin(vector<obj>& arr, int n, int i) {
-    int min = i;
-    int l = 2 * i + 1;
-    int r = 2 * i + 2;
+void heapifyMin(vector<obj>& v, int n, int i)
+{
+    int min=i;
+    int l=2*i+1; // left child
+    int r=2*i+2; // right child
 
     // left child < parent
-    if (l < n && arr[l].value < arr[min].value)
+    if (l < n && v[l].value < v[min].value)
         min = l;
 
     // right child < parent
-    if (r < n && arr[r].value < arr[min].value)
+    if (r < n && v[r].value < v[min].value)
         min = r;
 
     // parent is not the smallest
     if (min != i) {
-        swap(arr[i], arr[min]); // therefore swap with the smallest child
+        swap(v[i], v[min]); // therefore swap with the smallest child
 
         // another subtree
-        heapifyMin(arr, n, min);
+        heapifyMin(v, n, min);
     }
 }
 
 // descending order using heapify
 void buildMinHeap(vector<obj>& heap) {
-    // build min-heap.
-    for (int i = (heap.size()-1) / 2 - 1; i >= 0; i--)
+    // build min-heap
+    for (int i=(heap.size()-1) / 2 - 1; i >= 0; i--)
         heapifyMin(heap, heap.size(), i);
 
 }
 
-void adjustMinHeap(vector<obj>& heap) {
-
-
-        int s = heap.size() - 1;
-        int i = 0;
-        int j = 2 * i + 1;
-        int r = 2 * i + 2;
-        int minChild = 0;
-
-        swap(heap[0], heap[s]);
-
-        while (r <= s) // stop condition at the last element
-        {
-            if (r < s && heap[r].value < heap[j].value)
-                minChild = r;
-            else
-                minChild = j;
-
-            if (heap[i].value > heap[minChild].value) {
-                swap(heap[i], heap[minChild]);
-                i = minChild;
-                j = 2 * i + 1;
-                r = 2 * i + 2;
-            } else
-                break;
-        }
-        heap.pop_back();
-
-
-
-}
-
-
 // get the data from the file
-void getData(vector<obj>& arr) {
+void getData(vector<obj>& v) {
     ifstream file("input.txt");
     string line;
 
@@ -118,87 +86,81 @@ void getData(vector<obj>& arr) {
         int index = 0;
         while (getline(file, line)) // fill vector
         {
-            istringstream stream(line);
+            istringstream stream(line); //https://www.geeksforgeeks.org/processing-strings-using-stdistringstream/
             obj o;
-            stream >> o.date >> o.value;
-            arr.push_back(o);
+            stream>>o.date>>o.value; //using stream and >> will read until whitespace is reached
+            v.push_back(o);
             index++; // next element
         }
     }
-
     file.close();
 }
 
 // amount of change of the exchange rate from the mean M at a given day
-void updateValues(vector<obj>& arr, float mean) {
-    for (int i = 0; i < arr.size(); i++)
-        arr[i].value = arr[i].value - mean;
+void updateValues(vector<obj>& heap, float mean)
+{
+    for (int i = 0; i < heap.size(); i++)
+        heap[i].value = heap[i].value - mean;
 }
 
 // dates of each of the N most positive changes
-void mostPositive(vector<obj>& arr, int num) {
-    buildMaxHeap(arr);
+void mostPositive(vector<obj>& heap, int num)
+{
+    buildMaxHeap(heap);
 
 
     for (int i = 0; i < num; i++)
     {
-        obj d=arr.front();
-        swap(arr.front(),arr.back());
-        arr.pop_back();
-        heapifyMax(arr,arr.size(),0);
+        obj d=heap.front(); //d is now the front element of the array
+        swap(heap.front(),heap.back()); //swap first and last elements
+        heap.pop_back(); //delete last element
+        heapifyMax(heap,heap.size(),0); //adjust the heap after deletion
 
-        if (arr[i].value > 0)
-        {
-            cout << "Date: " << d.date << ", Value: " << d.value << endl;
-
-        }
+        if (heap[i].value > 0)
+            cout << "Date: " << d.date << ", Value: " << d.value << endl; 
         else
             break;
     }
 }
 
 // dates of each of the N most negative changes
-void mostNegative(vector<obj>& arr, int num)
+void mostNegative(vector<obj>& heap, int num)
 {
-    buildMinHeap(arr);
-
+    buildMinHeap(heap);
 
     for (int i = 0; i < num; i++)
     {
-        obj d=arr.front();
-        swap(arr.front(),arr.back());
-        arr.pop_back();
-        heapifyMin(arr,arr.size(),0);
+        obj d=heap.front(); //d is now the front element of the array
+        swap(heap.front(),heap.back()); //swap first and last elements
+        heap.pop_back(); //delete last element
+        heapifyMin(heap,heap.size(),0); //adjust the heap after deletion
 
-        if (arr[i].value < 0)
-        {
+        if (heap[i].value < 0)
             cout << "Date: " << d.date << ", Value: " << d.value << endl;
-
-        }
         else
             break;
     }
 }
 
 // calculate mean
-float mean(vector<obj>& arr) {
+float mean(vector<obj>& v) {
     float avg = 0.0;
-    for (int i = 0; i < arr.size(); i++)
-        avg += arr[i].value;
-    avg = avg / arr.size();
+    for (int i = 0; i < v.size(); i++)
+        avg += v[i].value;
+    avg = avg / v.size();
     return avg;
 }
 
 // maximum subsequence sum algorithm
-float maxSubSeq(vector<obj>& arr) {
+float maxSubSeq(vector<obj>& v) {
     int i = 1;
     int im = 0;
     int jm = 0;
     float Sij = 0;
     float Smax = 0;
 
-    for (int j = 1; j <= arr.size(); j++) {
-        Sij = Sij + arr[j].value;
+    for (int j = 1; j <= v.size(); j++) {
+        Sij = Sij + v[j].value;
         if (Sij > Smax) {
             Smax = Sij;
             im = i;
@@ -208,13 +170,13 @@ float maxSubSeq(vector<obj>& arr) {
             Sij = 0;
         }
     }
-    cout << "Start date is: " << arr[im].date << " End date is: " << arr[jm].date << endl;
+    cout << "Start date is: " << v[im].date << " End date is: " << v[jm].date << endl;
     return Smax;
 }
 
 int main() {
-    vector<obj> arr;
-    getData(arr);
+    vector<obj> heap;
+    getData(heap);
 
     int choice;
     cout << "Greetings!" << endl;
@@ -241,15 +203,15 @@ int main() {
         cin >> c;
         cout << "Moreover, how many values would you like" << endl;
         if (c == 1) {
-            float f = mean(arr);
-            updateValues(arr, f);
-            mostPositive(arr, 10);
+            float f = mean(heap);
+            updateValues(heap, f);
+            mostPositive(heap, 10);
         } else {
             float x;
             cout << "Please enter a mean of your own" << endl;
             cin >> x;
-            updateValues(arr, x);
-            mostPositive(arr, 10);
+            updateValues(heap, x);
+            mostPositive(heap, 10);
         }
         cout << "Done! Thank you!" << endl;
     } else if (choice == 2) {
@@ -263,15 +225,15 @@ int main() {
         cout << "Moreover, how many values would you like" << endl;
         cin >> number;
         if (c == 1) {
-            float e = mean(arr);
-            updateValues(arr, e);
-            mostNegative(arr, number);
+            float e = mean(heap);
+            updateValues(heap, e);
+            mostNegative(heap, number);
         } else {
             float p = 0.0;
             cout << "Please enter a mean of your own" << endl;
             cin >> p;
-            updateValues(arr, p);
-            mostNegative(arr, number);
+            updateValues(heap, p);
+            mostNegative(heap, number);
         }
         cout << "Done! Thank you!" << endl;
     } else if (choice == 3) {
@@ -285,21 +247,21 @@ int main() {
         cout << "Moreover, how many values would you like" << endl;
         cin >> number;
         if (c == 1) {
-            float e = mean(arr);
-            updateValues(arr, e);
+            float e = mean(heap);
+            updateValues(heap, e);
             cout << "Top positive " << number << " changes are " << endl;
-            mostPositive(arr, number);
+            mostPositive(heap, number);
             cout << "Top negative " << number << " changes are " << endl;
-            mostNegative(arr, number);
+            mostNegative(heap, number);
         } else {
             float x;
             cout << "Please enter a mean of your own" << endl;
             cin >> x;
             cout << "Top positive " << number << " changes are " << endl;
-            updateValues(arr, x);
-            mostPositive(arr, number);
+            updateValues(heap, x);
+            mostPositive(heap, number);
             cout << "Top negative " << number << " changes are " << endl;
-            mostNegative(arr, number);
+            mostNegative(heap, number);
         }
         cout << "Done! Thank you!" << endl;
     }   else if(choice==4)
@@ -312,10 +274,10 @@ int main() {
         cin >> c;
         if(c==1)
         {
-            float e= mean(arr);
+            float e= mean(heap);
             float g=0.0;
-            updateValues(arr,e);
-            g= maxSubSeq(arr);
+            updateValues(heap,e);
+            g= maxSubSeq(heap);
             cout<<endl;
             cout<<"The summation between those dates is "<<g<<endl;
 
@@ -326,8 +288,8 @@ int main() {
             float y=0.0;
             cout<<"Please enter a mean of your own"<<endl;
             cin>>x;
-            updateValues(arr,x);
-            y= maxSubSeq(arr);
+            updateValues(heap,x);
+            y= maxSubSeq(heap);
             cout<<endl;
             cout<<"The summation between those dates is "<<y<<endl;
 
@@ -347,12 +309,12 @@ int main() {
         cin >> number;
         if (c == 1)
         {
-            float e = mean(arr);
-            updateValues(arr, e);
+            float e = mean(heap);
+            updateValues(heap, e);
             cout<<"Top positive "<<number<<" changes are "<<endl;
-            mostPositive(arr, number);
+            mostPositive(heap, number);
             cout<<"Top negative "<<number<<" changes are "<<endl;
-            mostNegative(arr, number);
+            mostNegative(heap, number);
         }
         else
         {
@@ -360,16 +322,16 @@ int main() {
             cout << "Please enter a mean of your own" << endl;
             cin >> x;
             cout << "Top positive " << number << " changes are " << endl;
-            updateValues(arr, x);
-            mostPositive(arr, number);
+            updateValues(heap, x);
+            mostPositive(heap, number);
             cout << "Top negative " << number << " changes are " << endl;
-            mostNegative(arr, number);
+            mostNegative(heap, number);
         }
 
         cout<<endl<<endl;
         cout<<"Now to get the start date and the end date of the contiguous period over which the sum of rate changes is maximum"<<endl;
 
-        getData(arr); //initialize
+        getData(heap); //initialize
         int b;
         cout << "Would you like to use the average as the mean here or would you like to insert a number of your own"
              << endl;
@@ -378,10 +340,10 @@ int main() {
         cin >> b;
         if(c==1)
         {
-            float e= mean(arr);
+            float e= mean(heap);
             float g=0.0;
-            updateValues(arr,e);
-            g= maxSubSeq(arr);
+            updateValues(heap,e);
+            g= maxSubSeq(heap);
             cout<<endl;
             cout<<"The summation between those dates is "<<g<<endl;
 
@@ -392,8 +354,8 @@ int main() {
             float y=0.0;
             cout<<"Please enter a mean of your own"<<endl;
             cin>>x;
-            updateValues(arr,x);
-            y= maxSubSeq(arr);
+            updateValues(heap,x);
+            y= maxSubSeq(heap);
             cout<<endl;
             cout<<"The summation between those dates is "<<y<<endl;
 
